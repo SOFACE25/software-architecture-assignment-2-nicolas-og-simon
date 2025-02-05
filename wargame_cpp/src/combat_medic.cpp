8fa71d64fde39e4785a58548d651e29f5bb915b3
Nicolas Berwald Sørensen <nicolassorensen80@gmail.com>
2025-02-05 19:50:07 +0100

    Fixed c and cpp files for medic and solider. All tests passed

--- wargame_cpp/src/combat_medic.cpp
+++ wargame_cpp/src/combat_medic.cpp
@@ -4,15 +4,21 @@ CombatMedic::CombatMedic(int health, int damage) : Soldier(health, damage)
 {
 }
 
+// Implementation of the pure-virtual heal function:
 void CombatMedic::heal(Unit &target)
 {
+    // Saving the current health from the target that the CombatMedic wishes to heal
     int current_health = target.get_health();
+
+    // Checks if the target is already dead.
     if (current_health <= 0)
     {
-        //cant heal if target is dead.
+        // Can't heal the target if it is already dead.
     }
+    // If the target is not dead:
     else
     {
+        // Incrementing the health of the target by 10:
         target.set_health(current_health + 10);
     }
 }
--- wargame_cpp/src/soldier.cpp
+++ wargame_cpp/src/soldier.cpp
@@ -4,11 +4,14 @@ Soldier::Soldier(int health, int damage) : Unit(health), damage(damage)
 {
 }
 
+// Implementation of the virtual function attack from class Unit:
 void Soldier::attack(Unit &target)
 {
+    // Affecting the damage from a soldier to the target.
+    // So after this the new health of the target will be: get_health() - damage
     target.take_hit(damage);
 }
 void Soldier::heal(Unit &target)
 {
-    //soliders cant heal
+    // Soldiers are not able to heal. Therefore just a default implementation.
 }
