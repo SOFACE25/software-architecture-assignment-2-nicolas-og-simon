e61f240155d1d8399fcbc81467bc946ba4bda197
Simon <simongronseth@gmail.com>
2025-02-05 14:48:56 +0100

    Fixed c and cpp files for medic and solider. all test passed

--- wargame_cpp/src/combat_medic.cpp
+++ wargame_cpp/src/combat_medic.cpp
@@ -6,4 +6,13 @@ CombatMedic::CombatMedic(int health, int damage) : Soldier(health, damage)
 
 void CombatMedic::heal(Unit &target)
 {
+    int current_health = target.get_health();
+    if (current_health <= 0)
+    {
+        //cant heal if target is dead.
+    }
+    else
+    {
+        target.set_health(current_health + 10);
+    }
 }
--- wargame_cpp/src/soldier.cpp
+++ wargame_cpp/src/soldier.cpp
@@ -6,7 +6,9 @@ Soldier::Soldier(int health, int damage) : Unit(health), damage(damage)
 
 void Soldier::attack(Unit &target)
 {
+    target.take_hit(damage);
 }
 void Soldier::heal(Unit &target)
 {
+    //soliders cant heal
 }
